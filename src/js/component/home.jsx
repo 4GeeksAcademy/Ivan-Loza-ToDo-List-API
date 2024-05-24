f4ddb01c383c82f856a064b1db069b5f0239f17f
Iván Loza <ivanloza@Ivans-Laptop.local>
2024-05-24 17:41:47 -0500

    Added todo list code

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,77 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [inputValue, setInputValue] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  function addTask() {
+    if (inputValue !== "") {
+      const capitalizedTask =
+        inputValue.charAt(0).toUpperCase() + inputValue.slice(1);
+      setTasks([...tasks, capitalizedTask]);
+      setInputValue("");
+    }
+  }
+
+  function removeTask(indexToRemove) {
+    setTasks(tasks.filter((task, arrayIndex) => arrayIndex !== indexToRemove));
+  }
+
+  function tasksLeft() {
+    const tasksToDo = tasks.length;
+    if (tasksToDo === 0) {
+      return "Nothing to do...";
+    }
+    if (tasksToDo === 1) {
+      return "Just " + tasksToDo + " task left to do!";
+    }
+    if (tasksToDo > 0) {
+      return "Just " + tasksToDo + " tasks left to do!";
+    }
+  }
+
+  return (
+    <div className="container-fluid d-flex justify-content-center">
+      <div className="mt-5">
+        <h1 className=" display-1 ">TO-DO LIST</h1>
+        <div className="containter d-flex ">
+          <input
+            className="form-control"
+            id="input"
+            placeholder="What needs to be done?"
+            type="text"
+            value={inputValue}
+            onChange={(event) => setInputValue(event.target.value)}
+            onKeyDown={(event) => {
+              if (event.key === "Enter") {
+                addTask();
+              }
+            }}
+          ></input>
+        </div>
+        <ul className="list-group mt-2">
+          {tasks.map((task, index) => (
+            <li
+              key={index}
+              className="list-group-item d-flex justify-content-between align-items-center "
+            >
+              {task}
+              <button
+                type="sm-button"
+                className="removeButton btn btn-ligth btn-sm"
+                onClick={() => removeTask(index)}
+              >
+                X
+              </button>
+            </li>
+          ))}
+        </ul>
+
+        <p className="tasksLeft mt-1 ms-3">{tasksLeft()}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
